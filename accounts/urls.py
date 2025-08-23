2bf97a1660a490712f4690543d3e3b3a33afaf1a
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-08-23 21:01:12 +0300

    Add accounts app with profiles and auth forms

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .forms import LoginForm
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="accounts/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
+]
