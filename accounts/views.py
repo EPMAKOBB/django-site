cca5102f75d7e6c80e3bc1cdde6a56a5413d3784
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-08-30 09:41:55 +0300

    Add user dashboard and custom signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import redirect, render
 
-from .forms import SignupForm
+from .forms import SignupForm, UsernameChangeForm
 
 
 def signup(request):
@@ -15,3 +17,29 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, "accounts/signup.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    if request.method == "POST":
+        if "username_submit" in request.POST:
+            u_form = UsernameChangeForm(request.POST, instance=request.user)
+            p_form = PasswordChangeForm(request.user)
+            if u_form.is_valid():
+                u_form.save()
+                return redirect("accounts:dashboard")
+        elif "password_submit" in request.POST:
+            u_form = UsernameChangeForm(instance=request.user)
+            p_form = PasswordChangeForm(request.user, request.POST)
+            if p_form.is_valid():
+                user = p_form.save()
+                update_session_auth_hash(request, user)
+                return redirect("accounts:dashboard")
+    else:
+        u_form = UsernameChangeForm(instance=request.user)
+        p_form = PasswordChangeForm(request.user)
+    return render(
+        request,
+        "accounts/dashboard.html",
+        {"u_form": u_form, "p_form": p_form},
+    )
