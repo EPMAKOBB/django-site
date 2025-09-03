955470eabe0f1ec6e9e3692e19ff776132ddf822
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-09-03 14:36:04 +0300

    Add teachers dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -30,3 +30,10 @@ def progress(request):
     )
     context = {"skill_masteries": masteries, "active_tab": "progress"}
     return render(request, "accounts/dashboard.html", context)
+
+
+@login_required
+def dashboard_teachers(request):
+    """Display a placeholder teachers dashboard."""
+    context = {"active_tab": "teachers"}
+    return render(request, "accounts/dashboard/teachers.html", context)
