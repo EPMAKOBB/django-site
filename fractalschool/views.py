11011dd6620b267aa193ab29fac90a393cee68c7
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-09-06 20:24:53 +0300

    Add ApplicationForm and render on home page

--- fractalschool/views.py
+++ fractalschool/views.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
 
+from applications.forms import ApplicationForm
+
 
 class HomeView(TemplateView):
     """Render the main landing page."""
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = ApplicationForm()
+        return context
 
