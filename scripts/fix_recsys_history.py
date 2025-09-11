4a17d9867190224be02dc95c918feef22879823d
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-09-11 09:49:45 +0300

    Test automatic deployment

---
+++ scripts/fix_recsys_history.py
@@ -0,0 +1,7 @@
+﻿import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fractalschool.settings")
+django.setup()
+from django.db import connection
+with connection.cursor() as c:
+    c.execute("DELETE FROM django_migrations WHERE app=%s", ['recsys'])
+print("✅ recsys history cleared")
