e70a2688f992d91d7d21a05c35a3f9b4dfedc594
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-09-18 16:00:54 +0300

    Add exam versions relationship to student profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import StudentProfile, TeacherProfile
 
 
-admin.site.register(StudentProfile)
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ('exam_versions',)
+
+
 admin.site.register(TeacherProfile)
