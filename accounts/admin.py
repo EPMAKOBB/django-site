ca6a4c355fb0b3342c0fdb4b056a7765f0ead01c
EPMAKOBB <134048869+EPMAKOBB@users.noreply.github.com>
2025-09-24 19:37:42 +0300

    Add course and enrollment models

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from .models import StudentProfile, TeacherProfile
 
 
-admin.site.register(StudentProfile)
-admin.site.register(TeacherProfile)
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    search_fields = (
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
+    autocomplete_fields = ("exam_versions",)
+
+
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    search_fields = (
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
